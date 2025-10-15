089392aef29b677e193433a759416541ff798170
kjmumtaz <kjmumtaz@umich.edu>
2025-10-15 18:58:12 -0400

    Debugged csv reader class to fix errors

--- CodeMain.py
+++ CodeMain.py
@@ -20,23 +20,30 @@ class ImportPenguins():
         header = next(reader)
 
         for row in reader:
-            species = row[0]
-            island = row[1]
-            bill_length_mm = row[2]
-            bill_depth_mm = row[3]
-            flipper_length_mm = row[4]
-            body_mass_g = row[5]
+            species = row[1]
+            island = row[2]
+            bill_length_mm = row[3]
+            bill_depth_mm = row[4]
+            flipper_length_mm = row[5]
+            body_mass_g = row[6]
 
             if island not in PenguinData:
                 PenguinData[island] = {}
             
             if species not in PenguinData[island]:
-                PenguinData[island][species] = {bill_depth_mm: [], bill_length_mm: [], flipper_length_mm: [], body_mass_g: []}
+                PenguinData[island][species] = {
+                    "bill_depth_mm": [],
+                    "bill_length_mm": [],
+                    "flipper_length_mm": [],
+                    "body_mass_g": []
+                }
 
-            PenguinData[island][species][bill_depth_mm].append(float(bill_depth_mm))
-            PenguinData[island][species][bill_length_mm].append(float(bill_length_mm))
-            PenguinData[island][species][flipper_length_mm].append(float(flipper_length_mm))
-            PenguinData[island][species][body_mass_g].append(float(body_mass_g)) 
+            if bill_depth_mm and bill_length_mm and flipper_length_mm and body_mass_g != "NA":
+
+                PenguinData[island][species]["bill_depth_mm"].append(float(bill_depth_mm))
+                PenguinData[island][species]["bill_length_mm"].append(float(bill_length_mm))
+                PenguinData[island][species]["flipper_length_mm"].append(float(flipper_length_mm))
+                PenguinData[island][species]["body_mass_g"].append(float(body_mass_g)) 
 
 
 
@@ -45,3 +52,4 @@ class ImportPenguins():
 
 class Main():
     ImportPenguins()
+    print(ImportPenguins.PenguinData)
